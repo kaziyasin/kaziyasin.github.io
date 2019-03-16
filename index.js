893204b3617cbfbfb923de5a1e34fe38122972af
Kazi Yasin Islam <kaziyasin2013@gmail.com>
2019-03-17 01:42:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -393,25 +393,6 @@
             window.addEventListener(Runner.events.RESIZE,
                 this.debounceResize.bind(this));
 
-	    window.addEventListener('load', function () {
-        	var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-	        var source = audioCtx.createBufferSource();
-        	var xhr = new XMLHttpRequest();
-	        xhr.open('GET', 'https://docs.google.com/uc?export=download&id=1zkrhV0uUKVzhkDRcVZ6iRFFhg9ftyTek');
-        	xhr.responseType = 'arraybuffer';
-	        xhr.addEventListener('load', function (r) {
-        	    audioCtx.decodeAudioData(
-                    	xhr.response, 
-                    	function (buffer) {
-                       		source.buffer = buffer;
-                        	source.connect(audioCtx.destination);
-                        	source.loop = false;
-                    	});
-            	    source.start(0);
-        	});
-        	xhr.send();
-   	    });
-
         },
 
         /**
