c920cfa157b3623cd2716ccd0e7426c1765d3d35
Kazi Yasin Islam <kaziyasin2013@gmail.com>
2019-03-18 18:52:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -308,9 +308,9 @@
          * Load and decode base 64 encoded sounds. (CHANGE)
          */
         loadSounds: function () {
-	    var track = new Audio('https://docs.google.com/uc?export=download&id=1zkrhV0uUKVzhkDRcVZ6iRFFhg9ftyTek');
-	    track.play();
             if (!IS_IOS) {
+                var track = new Audio('https://docs.google.com/uc?export=download&id=1zkrhV0uUKVzhkDRcVZ6iRFFhg9ftyTek');
+                track.play();
                 this.audioContext = new AudioContext();
                 var resourceTemplate =
                     document.getElementById(this.config.RESOURCE_TEMPLATE_ID).content;
